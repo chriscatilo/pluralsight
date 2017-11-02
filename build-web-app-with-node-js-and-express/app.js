413b43a074e82d24399c653cef197a607fc16b78
chris <chris@foo.com>
2017-11-02 21:20:03 +0000

    add routing to /books

--- build-web-app-with-node-js-and-express/app.js
+++ build-web-app-with-node-js-and-express/app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-
-var app = express();
-
-var port = process.env.PORT || 5000;
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 5000,
+    bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views','src/views');
 app.set('view engine','ejs');
+
+bookRouter.route('/')
+    .get(function(req, res){
+        res.send('Hello Books');
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res){
+        res.send('Hello Single Book');
+    });
+
+app.use('/books', bookRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
