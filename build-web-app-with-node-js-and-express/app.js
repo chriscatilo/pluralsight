bdab277a2a3b325cbb36fad5d98811766f8e0ddc
chris <chris@foo.com>
2017-11-02 15:54:42 +0000

    build web app: hello world

--- build-web-app-with-node-js-and-express/app.js
+++ build-web-app-with-node-js-and-express/app.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+
 app.listen(port, function(err) {
    console.log('runnning server on port ' + 5000); 
 });
