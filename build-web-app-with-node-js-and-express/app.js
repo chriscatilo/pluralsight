b3d4291051c0c4a12855012dd4eec38e08aadcb3
chris <chris@foo.com>
2017-11-03 01:00:45 +0000

    use mssql db as source for books data

--- build-web-app-with-node-js-and-express/app.js
+++ build-web-app-with-node-js-and-express/app.js
@@ -1,6 +1,25 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 5000;
+    sql = require('mssql');
+
+
+/* mssql checklist
+1. Enable TCP/IP protocol from Computer Management/Services and Application/SQL Server Configuration
+2. Ensure that TCP Port for IPAll is set to 1433
+3. Windows Firewall (wf.msc) allows traffic in port 1433
+*/
+var sqlConfig = {
+    user: 'books',
+    password: 'p@ssw0rd1',
+    server: 'localhost',
+    port: 1433,
+    //debug: true,
+    database: 'kodigouk.pluralsight.build-web-app.books'
+};
+
+sql.connect(sqlConfig, function(err) {
+    console.log(err);
+});
 
 var nav = [
     {link: '/books', text: 'Book'},
@@ -23,6 +42,7 @@ app.get('/', function(req, res) {
         ]});
 });
 
+var port = process.env.PORT || 5000;
 app.listen(port, function(err) {
     console.log('runnning server on port ' + port);
 });
--- build-web-app-with-node-js-and-express/src/routes/bookRoutes.js
+++ build-web-app-with-node-js-and-express/src/routes/bookRoutes.js
@@ -1,73 +1,44 @@
-var express = require('express');
+var express = require('express'),
+    sql = require('mssql');
 
 var router = function(nav) {
     var bookRouter = express.Router();
-    var books = [
-        {
-            title: 'War and Peace',
-            genre: 'Historical Fiction',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
-        },
-        {
-            title: 'Les Misérables',
-            genre: 'Historical Fiction',
-            author: 'Victor Hugo',
-            read: false
-        },
-        {
-            title: 'The Time Machine',
-            genre: 'Science Fiction',
-            author: 'H. G. Wells',
-            read: false
-        },
-        {
-            title: 'A Journey into the Center of the Earth',
-            genre: 'Science Fiction',
-            author: 'Jules Verne',
-            read: false
-        },
-        {
-            title: 'The Dark World',
-            genre: 'Fantasy',
-            author: 'Henry Kuttner',
-            read: false
-        },
-        {
-            title: 'The Wind in the Willows',
-            genre: 'Fantasy',
-            author: 'Kenneth Grahame',
-            read: false
-        },
-        {
-            title: 'Life On The Mississippi',
-            genre: 'History',
-            author: 'Mark Twain',
-            read: false
-        },
-        {
-            title: 'Childhood',
-            genre: 'Biography',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
-        }];
-
     bookRouter.route('/')
         .get(function(req, res) {
-            res.render('bookListView', {
-                title: 'Books',
-                nav: nav,
-                books: books
+            var request = new sql.Request();
+            request.query('select * from books', function(err, recordset) {
+                res.render('bookListView', {
+                    title: 'Books',
+                    nav: nav,
+                    books: recordset
+                });
             });
+
+            
         });
 
     bookRouter.route('/:id')
+        .all(function(req, res, next) {
+            var id = req.params.id,
+                ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int);
+            ps.prepare('select * from books where id = @id', function(err){
+                ps.execute({id:req.params.id},
+                    function(err, recordset) {
+                        if (recordset.length === 0) {
+                            res.status(404).send('Not Found');
+                        } else {
+                            req.book = recordset[0];
+                            next();
+                        }
+                    });
+            });
+        })
         .get(function(req, res) {
-            var id = req.params.id;
             res.render('bookView', {
-                title: 'Books',
-                nav: nav,
-                book: books[id]
+                            title: 'Books',
+                            nav: nav,
+                            book: req.book
             });
         });
 
